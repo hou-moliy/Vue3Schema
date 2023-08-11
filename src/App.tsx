a27ac5d2c1bd70ef98aa052954b950877d96be40
houyuanzhen <houyuanzhen@ebupt.com>
2023-08-11 14:56:40 +0800

    [Feat]: 新增monaco-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { defineComponent, reactive } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   setup() {
@@ -11,15 +10,12 @@ export default defineComponent({
     //   state.age++;
     //   state.name = "hy" + state.age;
     // }, 1000);
-    const renderHelloWorld = (msg: string, age: number) => {
-      return <HelloWorld msg={msg} age={age} />;
-    };
+
     return () => {
       return (
         <div id="app">
           <input type="text" v-model={state.name} />
           <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-          {renderHelloWorld(state.name, state.age)}
         </div>
       );
     };
