66fb9349a304dca512a8e873e648a51730ffa0ae
houyuanzhen <houyuanzhen@ebupt.com>
2023-08-23 11:34:17 +0800

    [Feat]: 增加utils.ts，修改type.ts，复杂节点的渲染介绍

--- lib/types.ts
+++ lib/types.ts
@@ -12,24 +12,37 @@ type SchemaRef = { $ref: string };
 
 // type Schema = any
 export interface Schema {
-  type: SchemaTypes | string; // 决定我们如何渲染
+  type?: SchemaTypes | string; // 决定我们如何渲染
   const?: any;
   format?: string;
+  title?: string;
   default?: any;
   properties?: {
     [key: string]: Schema | { $ref: string };
   };
   items?: Schema | Schema[] | SchemaRef;
+  uniqueItems?: any;
   dependencies?: {
     [key: string]: string[] | Schema | SchemaRef;
   };
   oneOf?: Schema[];
+  anyOf?: Schema[];
+  allOf?: Schema[];
+  // TODO：uiSchema
   // vjsf?: VueJsonSchemaConfig
   required?: string[];
   enum?: any[];
+  enumNames?: any[];
   enumKeyValue?: any[];
   additionalProperties?: any;
   additionalItems?: Schema;
+  minLength?: number;
+  maxLength?: number;
+  minimun?: number;
+  maximum?: number;
+  multipleOf?: number;
+  exclusiveMaximum?: number;
+  exclusiveMinimum?: number;
 }
 
 export const FiledPropsDefine = {
